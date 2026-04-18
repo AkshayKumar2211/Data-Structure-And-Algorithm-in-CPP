2a235ec5b5eee7ea193424a4b398dc42062c0480
Akshay Kumar <taara0361@gmai.com>
2026-04-18 12:01:53 +0530

    stack shot

---
+++ stack/implemenmtKStack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class mystack{
+    int capacity;
+    int *arr;
+    int top=-1;
+
+    public:
+    mystack(int capacity)
+    {
+        int capacity=capacity;
+        arr=new int[capacity];
+        int top=-1
+    }
+
+    void 
+};
+
+
+int main()
+{
+
+}
